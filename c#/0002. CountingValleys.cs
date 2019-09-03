26e0eff3691361e89c5a793e293ad6454d55ff8b
Irina Hristova <hristova119@gmail.com>
2019-09-03 23:30:51 +0300

    feat: add sln to 0002.

---
+++ c#/0002. CountingValleys.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace counting_valleys
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 8;
+            string s = "UDDDUDUU";
+
+            Console.WriteLine(FindValleys(n, s));
+        }
+
+        private static int FindValleys(int n, string s)
+        {
+            int v = 0; // valleys
+            int lvl = 0; // level
+            var arr = s.ToCharArray();
+
+            foreach (char c in arr)
+            {
+                if (c == 'U') ++lvl;
+                if (c == 'D') --lvl;
+
+                if (lvl == 0 && c == 'U')
+                {
+                    ++v;
+                }
+            }
+
+            return v;
+        }
+    }
+}
