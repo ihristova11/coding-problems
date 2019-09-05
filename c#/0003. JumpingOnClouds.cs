32554c6e4d081787d97c5a141b08bb78363fb427
Irina Hristova <hristova119@gmail.com>
2019-09-05 21:49:42 +0300

    feat: c# sln to 0003.JumpingOnClouds

---
+++ c#/0003. JumpingOnClouds.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace jumping_on_the_clouds
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 7;
+
+            int[] arr = { 0, 0, 1, 0, 0, 1, 0 };
+
+            Console.WriteLine(FindJumps(arr));
+        }
+
+        private static int FindJumps(int[] c)
+        {
+            int count = -1;
+            for (int i = 0; i < c.Length; i++, count++)
+            {
+                if (i < c.Length - 2 && c[i + 2] == 0) i++;
+            }
+
+            return count;
+        }
+    }
+}
