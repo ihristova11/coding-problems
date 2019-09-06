7a429e9f5af614703a6935ecb39c34769d4cc2c7
Irina Hristova <hristova119@gmail.com>
2019-09-06 21:33:13 +0300

    feat: c# sln for 0004.RepeatedStrings

---
+++ c#/warm-up/0004.RepeatedStrings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace repeated_strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = "abcac";
+            long n = 10;
+            Console.WriteLine(FindOccurances(s, n));
+        }
+
+        private static long FindOccurances(string s, long n)
+        {
+            long len = s.Length;
+            long counter = 0;
+
+            long stringRepetition = n / len;
+            long remaining = n % len;
+
+            for (int i = 0; i < len; i++)
+            {
+                if(s[i] == 'a')
+                {
+                    counter++;
+                }
+            }
+
+            counter *= stringRepetition;
+
+            for (int i = 0; i < remaining; i++)
+            {
+                if(s[i] == 'a')
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
